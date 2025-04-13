154a4baf595385ac4e39970541670ed2e21878ed
samiabat <samuelabatneh20@gmail.com>
2025-04-13 23:49:34 +0300

    add the correct model path

--- config.py
+++ config.py
@@ -10,9 +10,15 @@ is_half = True if is_half_str.lower() == 'true' else False
 is_share_str = os.environ.get("is_share","False")
 is_share= True if is_share_str.lower() == 'true' else False
 
-cnhubert_path = "GPT_SoVITS/pretrained_models/chinese-hubert-base"
-bert_path = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
+cnhubert_path = "GPT_SoVITS/pretrained_models/japanese-hubert-base"
+# cnhubert_path="rinna/japanese-hubert-base"
+
+bert_path = "GPT_SoVITS/pretrained_models/bert-base-japanese-whole-word-masking"
+# bert_path = "tohoku-nlp/bert-base-japanese-whole-word-masking"
+
 pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2G488k.pth"
+# pretrained_sovits_path =  "AkitoP/GPT-SoVITS-JA-ProsodyControl_model/s2G488k.pth"
+
 pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
 
 exp_root = "logs"
